9192f60c34bfe56e170778c12f9042c69f5d8694
Omri Mor <omri_mor@msn.com>
2017-01-12 18:15:16 -0800

    added ProjectSchema in Project.js

--- server/models/Project.js
+++ server/models/Project.js
@@ -2,5 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProjectSchema = new Schema({
-    
+  title: {type: String, required: true, minlength: 1},
+  blurb: {type: String, required: true, maxlength: 500},
+  detail: {type: String, minlength: 1},
+  _skills: [{type: Schema.Type.ObjectId, ref: 'Skill'}],
+  code: {type: String, required: true, minlength:1}
 });
+
+console.log("Loaded project model");
