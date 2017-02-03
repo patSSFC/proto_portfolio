f046c60a1218bc79e737bd049413e5b669d26a38
Pat Yolian <pyolian@gmail.com>
2017-02-02 17:59:21 -0800

    add fields to project partial

--- client/scripts/controllers/client.controllers.projects.js
+++ client/scripts/controllers/client.controllers.projects.js
@@ -1,7 +1,8 @@
-angular.module('mainApp').controller('projectCtrl', function($scope, $location) {
+angular.module('mainApp').controller('projectCtrl', function($scope, $location, userFactory) {
     const vm = this;
-
+    
+    vm.user = userFactory.user;
     vm.createProject = function() {
-        
+
     }
 });
