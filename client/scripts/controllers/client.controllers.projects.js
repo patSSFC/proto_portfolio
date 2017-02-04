c2ff9bcfb75bb4c4c05c2c526c67bc219c993520
Pat Yolian <pyolian@gmail.com>
2017-02-03 17:01:32 -0800

    initiate createProject function

--- client/scripts/controllers/client.controllers.projects.js
+++ client/scripts/controllers/client.controllers.projects.js
@@ -1,6 +1,6 @@
 angular.module('mainApp').controller('projectCtrl', function($scope, $location, userFactory) {
     const vm = this;
-    
+
     vm.user = userFactory.user;
     vm.createProject = function() {
 
